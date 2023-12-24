81405b90541864221b5d7a446da1bd7ec55c24fc
dhokabeatz <amglna2020@gmail.com>
2023-12-23 20:27:00 -0500

    executable

--- 2-execute_a_command.pp
+++ 2-execute_a_command.pp
@@ -1,5 +1,9 @@
-# Executes a bash command
-exec { 'kill':
-  command => 'pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin']
+# 2-execute_a_command.pp
+
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => '/usr/bin:/bin',
+  logoutput   => true,
+  refreshonly => true,
 }
+
