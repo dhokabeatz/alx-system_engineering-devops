906c8560a8ef871b85cdeeb70e25e0a32892d876
dhokabeatz <amglna2020@gmail.com>
2023-12-23 19:56:11 -0500

    Install fask file reviewed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-# Using Puppet, install flask from pip3
+#This manifest installs Flask version 2.1.0 using pip3
+
+# Ensure that the package is present
 package { 'flask':
-  ensure   => '2.1.0',
+  ensure => '2.1.0',
   provider => 'pip3',
 }
+
