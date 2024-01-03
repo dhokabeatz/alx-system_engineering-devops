a992f010eebc76578a2d20f6e1948c4029df84a9
dhokabeatz <amglna2020@gmail.com>
2024-01-03 14:03:14 -0500

    Puppet applied

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+include stdlib
+
+# Puppet manifest for SSH client configuration
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+}
+
