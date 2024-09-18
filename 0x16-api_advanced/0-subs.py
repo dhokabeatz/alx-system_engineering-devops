c061a82d470b3743eebc24cc0223713e8c2ab694
dhokabeatz <amglna2020@gmail.com>
2024-09-18 19:31:47 -0400

    Subs file altered

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
+"""
+number of subscribers for a given subreddit
+"""
 
-from json import loads
 from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """ecursive function that queries the Reddit API and returns a list
-    containing the titles of all hot articles for a given subreddit. If no
-    results are found for the given subreddit, the function should return None
     """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    response = get(url, headers=headers)
-    reddits = response.json()
+    response = get(url, headers=user_agent)
+    results = response.json()
 
     try:
-        subscribers = reddits.get('data').get('subscribers')
-        return int(subscribers)
-    except:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
